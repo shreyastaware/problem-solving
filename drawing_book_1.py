db038d14e3702c0f3116e4348e19e66fac65f5b5
Shreyas Taware <shreyastaware13@gmail.com>
2022-03-22 23:21:14 +0530

    changed to floor division

--- drawing_book_1.py
+++ drawing_book_1.py
@@ -1,10 +1,10 @@
 """
 The student wants to turn a minimum number of pages to get to page number `p` from either page number `1` or 
-page number `n`. The number of pages they must turn from page number `1` is always `p/2`, but there is a 
+page number `n`. The number of pages they must turn from page number `1` is always `p//2`, but there is a 
 twist when they start from the last page, page `n`:
 
-If `n` is odd, then they turn `(n-p)/2` pages.
-If `n` is even, then they turn `(n-p+1)/2` pages.
+If `n` is odd, then they turn `(n-p)//2` pages.
+If `n` is even, then they turn `(n-p+1)//2` pages.
 In Python 3, integer (floor) division makes it more simple:
 """
 
